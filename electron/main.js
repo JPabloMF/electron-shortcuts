fe9d53d27184fa272d8a911b0484bb700f3c77a4
JpabloMF <juanpablo45671@gmail.com>
2020-12-31 17:01:32 -0500

    Tray, tray logo and top menu bar added * Tray, tray logo and tray initial options added without functionality * Top menu bar added but its options were disabled temporarily

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, Tray } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
+const AppMenu = Menu;
 
 let mainWindow;
+let tray = null;
+const iconPath = path.join(__dirname, 'assets/logo192.png');
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -21,4 +24,142 @@ function createWindow() {
     mainWindow = null;
   });
 }
-app.on('ready', createWindow);
+
+function createMenu() {
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Exit',
+        },
+      ],
+    },
+    {
+      label: 'Manager',
+      submenu: [
+        {
+          label: 'Products',
+        },
+        {
+          label: 'Companies',
+        },
+        {
+          label: 'Customers',
+        },
+        {
+          label: 'Providers',
+        },
+        {
+          label: 'Others',
+          submenu: [
+            {
+              label: 'Families',
+            },
+            {
+              label: 'IVA',
+            },
+            {
+              label: 'Pallets',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'Sales',
+      submenu: [
+        {
+          label: 'Make sales',
+        },
+        {
+          label: 'Returns on sales',
+        },
+        {
+          label: 'Sales details',
+        },
+      ],
+    },
+    {
+      label: 'Inventories',
+      submenu: [
+        {
+          label: 'Initial inventory entry',
+        },
+        {
+          label: 'Check stocks',
+        },
+      ],
+    },
+    {
+      label: 'Purchases',
+      submenu: [
+        {
+          label: 'Make purchases',
+        },
+        {
+          label: 'Returns on purchases',
+        },
+      ],
+    },
+    {
+      label: 'Tools',
+      submenu: [
+        {
+          label: 'Skins',
+          submenu: [
+            {
+              label: 'Black',
+            },
+            {
+              label: 'White',
+            },
+          ],
+        },
+        {
+          label: 'Sign off',
+        },
+        {
+          label: 'Report designer',
+        },
+      ],
+    },
+    {
+      label: 'Window',
+      submenu: [
+        {
+          label: 'Cascade',
+        },
+        {
+          label: 'Horizontal alignment',
+        },
+        {
+          label: 'Vertical alignment',
+        },
+      ],
+    },
+    {
+      label: 'Help',
+    },
+  ];
+
+  const menu = AppMenu.buildFromTemplate([]);
+  AppMenu.setApplicationMenu(menu);
+}
+
+function createTray() {
+  tray = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Settings' },
+    { label: 'Help' },
+    { label: 'Exit' },
+  ]);
+  tray.setToolTip('This is my application.');
+  tray.setContextMenu(contextMenu);
+}
+
+app.on('ready', function () {
+  createWindow();
+  createMenu();
+  createTray();
+});
