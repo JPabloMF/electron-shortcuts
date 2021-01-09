458ba1194b58d26698a682d273800d255e6fca98
JpabloMF <juanpablo45671@gmail.com>
2021-01-09 16:32:10 -0500

    initial views relocated to dashboard folder

---
+++ src/views/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopNav from '../../components/topNav';
+import SideNav from '../../components/sideNav';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <TopNav />
+      <SideNav />
+    </div>
+  );
+};
+
+export default Dashboard;
